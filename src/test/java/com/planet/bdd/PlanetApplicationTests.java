75d168bc05d7bd1b32c371141d83c113240f1820
Celio Roberto Martins de Matos <celio.matos@itbam.org.br>
2021-07-23 15:50:21 -0400

    quase acabdo

--- src/test/java/com/planet/bdd/PlanetApplicationTests.java
+++ src/test/java/com/planet/bdd/PlanetApplicationTests.java
@@ -1,11 +1,11 @@
-package com.planet;
+package com.planet.bdd;
+
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "classpath:features", glue = {"com.planet.steps"})
-//@ActiveProfiles(value = "test")
+@CucumberOptions(features = "classpath:features")
 public class PlanetApplicationTests {
 }
--- src/test/java/com/planet/bdd/steps/ValidateJobDTOCreateSteps.java
+++ src/test/java/com/planet/bdd/steps/ValidateJobDTOCreateSteps.java
@@ -1,14 +1,15 @@
-package com.planet.steps;
+package com.planet.bdd.steps;
 
-import com.planet.CucumberSpringConfiguration;
+import com.planet.bdd.CucumberSpringConfiguration;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
-import org.junit.Before;
 
 import java.util.LinkedList;
 import java.util.List;
+
 
 public class ValidateJobDTOCreateSteps extends CucumberSpringConfiguration {
 
@@ -19,19 +20,19 @@ public class ValidateJobDTOCreateSteps extends CucumberSpringConfiguration {
         list = new LinkedList<>();
     }
 
-    @Given("^um lance valido$")
+    @Given("um lance valido")
     public void dado_lance_valido() {
         list.add(1);
     }
 
-    @When("^propoe ao leilao$")
+    @When("propoe ao leilao")
     public void the_client_calls_job() {
         list.add(2);
     }
 
-    @Then("^o lance eh aceito")
+    @Then("o lance eh aceito")
     public void the_client_receives_status_code_of_200() {
-        Assert.assertEquals(2, list.size());
+        Assert.assertEquals(1, list.size());
     }
 
 }
